3fca58faa52f285e02595ef7d8cbcf9b1669dfc2
Richard Marko <rmarko@redhat.com>
2014-09-10 15:39:41 +0200

    add release.sh

---
+++ release.sh
@@ -0,0 +1,14 @@
+VER="0.$(( $( cat VERSION | cut -d\. -f 2 ) + 1 ))"
+echo "Releasing ${VER}"
+
+echo ${VER} > VERSION
+git add VERSION
+git commit -m "version ${VER}"
+git tag v${VER}
+git clean -fdx -e releasing
+
+git archive --format tar \
+  --prefix will-crash-${VER}/ \
+  --output will-crash-${VER}.tar.gz HEAD
+
+scp will-crash-${VER}.tar.gz 48.io:sources/will-crash/
